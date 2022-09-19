1347ab273049d28d794870b07e0eee7491a0335c
Gerson Velasco <gvelassco@gmail.com>
2022-09-19 12:40:49 -0500

    ajustes

--- backend/App/views/validacion_all.php
+++ backend/App/views/validacion_all.php
@@ -456,7 +456,8 @@
         $('.iframe').on('click',function(){
             var pdf = $(this).attr('data-pdf');
             var user_id = $(this).attr('data-user-id');
-            //alert(pdf);
+            alert(pdf);
+            alert(user_id);
 
             $('.cont-modal').html('<iframe src="https://registro.lasra-mexico.org/comprobantesPago/'+user_id+"/"+pdf+'" " style="width:100%; height:700px;" frameborder="0" ></iframe>');
         })
