bd2ad1bde3fb6cb4e9fb41e66aa2bc9af251109a
Gerson26 <gvelassco@gmail.com>
2022-11-09 22:23:17 -0600

    ajuste gafete becado

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -840,6 +840,17 @@ html;
 
                 $btn_constancia_congreso = '<a href="/Constancias/abrirConstanciaCongreso/'.base64_encode($value['user_id']).'/'.base64_encode($button_congreso[0]['id_producto']).'" class="btn bg-turquoise-1 btn-icon-only text-white" title="Imprimir Constancia '.$button_congreso[0]['nombre'].'" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Imprimir Constancia Impresa" target="_blank"><i class="fas fa-print"> </i></a>';
 
+            }else if($value['porcentaje_beca'] >= 100){
+
+                $impreso = GeneralDao::getImpresionGafeteTipo($value['user_id'],'Congreso');
+
+                if(!$impreso){
+                    $btn_congreso = '<a href="/RegistroAsistencia/abrirpdfGafete/'.$value['user_id'].'" class="btn bg-turquoise btn-icon-only text-white" title="Imprimir Gafete Congreso" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Imprimir Gafetes" target="_blank"><i class="fas fa-print"> </i></a>';
+                }else{
+                    $btn_congreso = '<a href="javascript:void(0)" class="btn bg-turquoise btn-icon-only text-white" title="Ya se ha impreso el gafete de congreso" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Imprimir Gafetes"><i class="fas fa-print"> </i></a>';
+                }
+
+                $btn_constancia_congreso = '<a href="/Constancias/abrirConstanciaCongreso/'.base64_encode($value['user_id']).'/'.base64_encode($button_congreso[0]['id_producto']).'" class="btn bg-turquoise-1 btn-icon-only text-white" title="Imprimir Constancia '.$button_congreso[0]['nombre'].'" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Imprimir Constancia Impresa" target="_blank"><i class="fas fa-print"> </i></a>';
             }else{
                 $btn_congreso = '';
                 $btn_constancia_congreso = '';
